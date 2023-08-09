d7a43505d21c42e528b08c1d048d4aab561a4c35
ZahraAnanta <zahrareefa@gmail.com>
2023-08-09 21:33:43 +0700

    portofolio tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container : {
+      center: true,
+      padding : '16px'
+    },
+    extend: {
+      colors : {
+        primary : '#14b8a6',
+        metal : '#172554',
+        pucet : '#64748b',
+      },
+      screens: {
+        '2xl' : '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
+
